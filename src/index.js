2bb1b388e1fa05ae96b11892092515e132c878b6
Enrique <octepc.97@gmail.com>
2022-09-24 11:56:49 -0500

    added the key trace functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const startGameButton = document.querySelector("#start-button");
 const board = document.querySelector("#horca").getContext("2d");
 let secretWord = "";
+let letters = [];
 
 let words = [
   "cuchara",
@@ -23,12 +24,33 @@ const chooseSecretWord = () => {
   console.log(secretWord);
 };
 
+const checkLettersInserted = (key) => {
+  let estado = false;
+  if (
+    (key >= 65 && letters.indexOf(key)) ||
+    (key <= 90 && letters.indexOf(key))
+  ) {
+    letters.push(key);
+    console.log(key);
+    return estado;
+  } else {
+    estado = true;
+    console.log(key);
+    return estado;
+  }
+};
+
 const startGame = () => (e) => {
   e.preventDefault();
   startGameButton.style.display = "none";
   chooseSecretWord();
   drawCanvas();
   drawLine();
+
+  document.onkeydown = (e) => {
+    let letter = e.key.toUpperCase();
+    checkLettersInserted(letter);
+  };
 };
 
 startGameButton.addEventListener("click", startGame());
